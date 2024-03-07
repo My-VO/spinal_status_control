c26a264a029bb94c17a8bd8c3e6eae02332b76c4
My VO <kathy.nguyen198@gmail.com>
2024-03-07 19:35:15 +0100

    add style status control

--- app/occupation/columns.js
+++ app/occupation/columns.js
@@ -1,21 +1,55 @@
+import Brightness1Icon from '@mui/icons-material/Brightness1';
+
 const columns =  [
     {
         field: 'room',
         headerName: 'ROOM',
-        width: 200,
-        editable: true
+        flex: 2,
+        renderCell: (params) => (params.value).toUpperCase(),
     },
     {
         field: 'floor',
         headerName: 'FLOOR',
-        width: 200,
-        editable: true
+        flex: 1,
+        renderCell: (params) => (params.value).toUpperCase(),
     },
     {
         field: 'occupation',
         headerName: 'OCCUPATION',
-        width: 200,
-        editable: true
+        flex: 1,
+        renderCell: (params) => {
+            const valueControl = params.row.occupation
+            .replaceAll(/(\r\n|\n|\r)/gm, '')
+            .trim()
+            .toUpperCase();
+      
+            return valueControl === "TRUE"
+                ? (
+                    <>
+                        <Brightness1Icon
+                        style={{ color: 'green', fontSize: 8, marginRight: 6 }}
+                        />
+                        <p style={{color : 'green'}}>{valueControl}</p>
+                    </>
+                ) 
+                : ( valueControl === "FALSE"
+                    ? (
+                        <>
+                            <Brightness1Icon
+                            style={{ color: 'red', fontSize: 8, marginRight: 6 }}
+                            />
+                            <p style={{color : 'red'}}>{valueControl}</p>
+                        </>
+                    )
+                    : (
+                        <>
+                            <Brightness1Icon
+                            style={{ color: 'gris', fontSize: 8, marginRight: 6 }}
+                            />
+                            <p style={{color : 'gris'}}>{valueControl}</p>
+                        </>
+                    ))
+          },
     }
 ];
 
